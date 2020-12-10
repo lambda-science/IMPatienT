141d826c60b3ed56d40dae72391d0eb43a4d0971
Corentin <7775862-Corentin_7@users.noreply.gitlab.com>
2020-12-10 16:14:22 +0100

    Update to Openseadragon + annotorious

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
+import deepzoom
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory, session
 from werkzeug.utils import secure_filename
 from PIL import Image
+import json
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 import pandas as pd
+
 UPLOAD_FOLDER = "uploads"
 REPORT_FOLDER = "results"
 ALLOWED_EXTENSIONS = {'tif', 'tiff', 'png', 'jpg', 'jpeg'}
@@ -21,26 +24,40 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def create_thumbnail(image_path):
-    image = Image.open(image_path)
-    image.thumbnail((600, 600))
-    image.save(image_path+'_thumbnail.jpg')
-
 def create_feature_list(config_file):
     feature = open(config_file, "r")
-    feature_list = [line.strip().replace(" ", "_") for line in feature.readlines()]
+    feature_list = [ (line.strip().replace(" ", "_"), line) for line in feature.readlines()]
     return feature_list
+
+def create_deepzoom_file(image_path):
+    # Specify your source image
+    SOURCE = image_path
+    # Create Deep Zoom Image creator with weird parameters
+    creator = deepzoom.ImageCreator(
+        tile_size=256,
+        tile_overlap=2,
+        tile_format="png",
+        image_quality=1,
+    )
+    # Create Deep Zoom image pyramid from source
+    creator.create(SOURCE, ""+image_path+".dzi")
+
+
+@app.route('/uploads/<path:filename>')
+def send_dzi(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/uploads/<filename>')
 def uploads(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/results/<filename>')
+@app.route('/results/<path:filename>')
 def get_report(filename):
     return send_from_directory(app.config['REPORT_FOLDER'], filename)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    session.clear()
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -55,7 +72,8 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            create_thumbnail(UPLOAD_FOLDER+"/"+filename)
+            create_deepzoom_file(UPLOAD_FOLDER+"/"+filename)
+            session["filename"] = filename
             return redirect(url_for('annot_page', filename=filename))
     return render_template('index.html')
 
@@ -68,9 +86,17 @@ def annot_page():
         if 'submit_button' in request.form:
             data = request.form.to_dict()
             session["info_annot"] = data
-            session["filename"] = filename
             return redirect(url_for('write_report'))
     return render_template('annot.html', filename=filename, thumbnail=filename+"_thumbnail.jpg", feature_list=feature_list)
+
+
+@app.route('/write_annot', methods=['POST'])
+def write_annot():
+    raw_data = request.get_data()
+    parsed = json.loads(raw_data)
+    with open('results/31_01_NADH-TR_x_400.jpg.json', 'a') as json_file:
+        json_file.write(json.dumps(parsed, indent=4, sort_keys=True))
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 @app.route('/results', methods=['GET', 'POST'])
 def write_report():
