631ab350e7532a72f5b41f0fc37b1c20bcc48642
Corentin <7775862-Corentin_7@users.noreply.gitlab.com>
2020-12-16 16:08:52 +0100

    refactoring for WTforms usage

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ from flask import Flask, flash, request, redirect, url_for, render_template, sen
 from werkzeug.utils import secure_filename
 from PIL import Image
 
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField
+from wtforms.validators import DataRequired, Required
+
 from glob import glob
 from src import deepzoom
 
 # Constant variable for folders and allowed extension
 UPLOAD_FOLDER = "uploads"
 REPORT_FOLDER = "results"
-ALLOWED_EXTENSIONS = {"tif", "tiff", "png", "jpg", "jpeg"}
+ALLOWED_EXTENSIONS = ["tif", "tiff", "png", "jpg", "jpeg"]
 
 # Flask app initialisation and configuration
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["REPORT_FOLDER"] = REPORT_FOLDER
 app.config["SECRET_KEY"] = "myverylongsecretkey"
-
-
-def allowed_file(filename):
-    """Check if the sumbmitted file name is in allowed extension"""
-    return "." in filename and \
-           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def create_feature_list(config_file):
@@ -59,14 +58,8 @@ def create_history_file():
 
 
 @app.route("/uploads/<path:filename>")
-def send_dzi(filename):
-    """Serve files located in subfolder inside uploads folder"""
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
-
-
-@app.route("/uploads/<filename>")
 def uploads(filename):
-    """Serve files located in uploads folder"""
+    """Serve files located in subfolder inside uploads folder"""
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
@@ -82,51 +75,105 @@ def upload_file():
     """Index page that is used to upload the image to the app and register patient ID.
     Redirect to the annotation page after a succesful upload.
     Also show the availiable file that already have been uploaded"""
+    class ImageForm(FlaskForm):
+        image = FileField(validators=[
+            FileRequired(),
+            FileAllowed(ALLOWED_EXTENSIONS,
+                        "Ce fichier n'est pas une image valide !")
+        ],
+                          render_kw={"class": "form-control-file"})
+        patient_ID = StringField('patient_ID',
+                                 validators=[DataRequired()],
+                                 render_kw={
+                                     "placeholder": "Identifiant Patient",
+                                     "class": "form-control"
+                                 })
+        submit = SubmitField('Upload',
+                             render_kw={"class": "btn btn-primary mb-2"})
+
     file_list = create_history_file()
-    if request.method == "POST":
-        # check if the post request has the file part
-        if "file" not in request.files or "patient_ID" not in request.form:
-            flash("No file part or patient_ID")
-            return redirect(request.url)
-        file = request.files["file"]
-        patient_ID = request.form["patient_ID"]
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == "":
-            flash("No selected file")
-            return redirect(request.url)
-        if file and allowed_file(file.filename) and patient_ID:
-            filename = secure_filename(patient_ID + "_" + file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            # Create the deep zoom image and save filename & patient ID from the form
-            create_deepzoom_file(UPLOAD_FOLDER + "/" + filename)
-            session["filename"] = filename
-            session["patient_ID"] = patient_ID
-            return redirect(
-                url_for("annot_page", filename=filename,
-                        patient_ID=patient_ID))
-    return render_template("index.html", file_list=file_list)
+    form = ImageForm()
+    if form.validate_on_submit():
+        file = form.image.data
+        patient_ID = form.patient_ID.data
+        filename = secure_filename(patient_ID + "_" + file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        # Create the deep zoom image
+        create_deepzoom_file(
+            os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return redirect(
+            url_for("annot_page", filename=filename, patient_ID=patient_ID))
+    return render_template("index.html", file_list=file_list, form=form)
 
 
 @app.route("/annot", methods=["GET", "POST"])
 def annot_page():
     """Render the annotation page after the upload of the initial image. 
     Redirects to the results page when the annotation form is submitted."""
-    filename = request.args.get("filename")
     session["filename"] = request.args.get("filename")
     session["patient_ID"] = request.args.get("patient_ID")
-    feature_list = create_feature_list("config/config_ontology.tsv")
-    if request.method == "POST":
-        if "submit_button" in request.form:
-            # When the form is submitted we store the form data in the session variable (cookie) under the "info_annot" key
-            data = request.form.to_dict()
-            session["info_annot"] = data
-            return redirect(url_for("write_report"))
+    feature_list = create_feature_list(
+        os.path.join("config", "config_ontology.tsv"))
+
+    class AnnotForm(FlaskForm):
+        patient_nom = StringField('patient_nom',
+                                  validators=[DataRequired()],
+                                  render_kw={
+                                      "placeholder": "Nom Patient",
+                                      "class": "form-control"
+                                  })
+        patient_prenom = StringField('patient_prenom',
+                                     validators=[DataRequired()],
+                                     render_kw={
+                                         "placeholder": "Prénom Patient",
+                                         "class": "form-control"
+                                     })
+        patient_id = StringField('patient_ID',
+                                 render_kw={
+                                     "placeholder": session["patient_ID"],
+                                     "class": "form-control",
+                                     "readonly": "True"
+                                 })
+        expert_name = StringField('expert_name',
+                                  validators=[DataRequired()],
+                                  render_kw={
+                                      "placeholder": "Nom du rapporteur",
+                                      "class": "form-control",
+                                  })
+        submit = SubmitField('Générer le rapport',
+                             render_kw={"class": "btn btn-primary mb-2"})
+
+        diagnostic = StringField('diagnostic',
+                                 validators=[DataRequired()],
+                                 render_kw={
+                                     "placeholder": "Diagnostique de Maladie",
+                                     "class": "form-control"
+                                 })
+
+    for feature in feature_list:
+        setattr(
+            AnnotForm, feature[0],
+            RadioField(feature[1],
+                       choices=[('1', 'Présent'), ('-1', 'Absent'),
+                                ('0', 'Incertain')],
+                       default='-1',
+                       validators=[DataRequired()]))
+    form = AnnotForm()
+    if form.validate_on_submit():
+        # Save form data in the session cookie
+        session["patient_nom"] = form.patient_nom.data
+        session["patient_prenom"] = form.patient_prenom.data
+        session["expert_name"] = form.expert_name.data
+        session["diagnostic"] = form.diagnostic.data
+        session["feature"] = {}
+        for feature in feature_list:
+            session["feature"][feature[0]] = form.data[feature[0]]
+        return redirect(url_for("write_report"))
     return render_template("annot.html",
-                           filename=filename,
-                           thumbnail=filename + "_thumbnail.jpg",
+                           filename=session["filename"],
                            feature_list=feature_list,
-                           patient_ID=session["patient_ID"])
+                           patient_ID=session["patient_ID"],
+                           form=form)
 
 
 @app.route("/write_annot", methods=["POST"])
@@ -138,19 +185,19 @@ def write_annot():
     parsed = json.loads(raw_data)
     # If no annotations yet: json file is created
     if os.path.exists("results/" + session["filename"] + ".json") == False:
-        with open("results/" + session["filename"] + ".json",
+        with open(os.path.join("results/", session["filename"] + ".json"),
                   "w") as json_file:
             annot_list.append(parsed)
             json_file.write(json.dumps(annot_list, indent=4))
     # If there are already some annotation: we add the new annotations to the file
     else:
         # First open as read only to load existing JSON
-        with open("results/" + session["filename"] + ".json",
+        with open(os.path.join("results/", session["filename"] + ".json"),
                   "r") as json_file:
             old_data = json.load(json_file)
             old_data.append(parsed)
         # Then open as write to overwrite the old file with old json+new data
-        with open("results/" + session["filename"] + ".json",
+        with open(os.path.join("results/", session["filename"] + ".json"),
                   "w") as json_file:
             json_file.write(json.dumps(old_data, indent=4))
     return json.dumps({"success": True}), 200, {
@@ -166,7 +213,8 @@ def update_annot():
     parsed = json.loads(raw_data)
     updated_list = []
     # First open as read only to load existing JSON
-    with open("results/" + session["filename"] + ".json", "r") as json_file:
+    with open(os.path.join("results/", session["filename"] + ".json"),
+              "r") as json_file:
         old_data = json.load(json_file)
         # Compare ID of annotation and replace the old annotations with the new one when there is a match in IDs.
         for anot in old_data:
@@ -175,7 +223,8 @@ def update_annot():
             elif parsed["id"] == anot["id"]:
                 updated_list.append(parsed)
     # Write the new annotations JSON data to file.
-    with open("results/" + session["filename"] + ".json", "w") as json_file:
+    with open(os.path.join("results/", session["filename"] + ".json"),
+              "w") as json_file:
         json_file.write(json.dumps(updated_list, indent=4))
     return json.dumps({"success": True}), 200, {
         "ContentType": "application/json"
@@ -190,14 +239,16 @@ def delete_annot():
     parsed = json.loads(raw_data)
     updated_list = []
     # First open as read only to load existing JSON
-    with open("results/" + session["filename"] + ".json", "r") as json_file:
+    with open(os.path.join("results/", session["filename"] + ".json"),
+              "r") as json_file:
         old_data = json.load(json_file)
         # If annotation ID is different from the ID of deletion command we save them to a list. Matching ID will be skipped and erased.
         for anot in old_data:
             if parsed["id"] != anot["id"]:
                 updated_list.append(anot)
     # Write the new annotations JSON data to file.
-    with open("results/" + session["filename"] + ".json", "w") as json_file:
+    with open(os.path.join("results/", session["filename"] + ".json"),
+              "w") as json_file:
         json_file.write(json.dumps(updated_list, indent=4))
     return json.dumps({"success": True}), 200, {
         "ContentType": "application/json"
@@ -207,35 +258,27 @@ def delete_annot():
 @app.route("/results", methods=["GET", "POST"])
 def write_report():
     """Write the histological report and serve the results page for patient with download links"""
-    # Access the Session cookie and pop out basic informations for the file header
-    prenom_patient = session["info_annot"].pop("prenom_patient")
-    nom_patient = session["info_annot"].pop("nom_patient")
-    id_patient = session["patient_ID"]
-    redacteur_rapport = session["info_annot"].pop("redacteur_rapport")
-    diag = session["info_annot"].pop("diag")
-    del session["info_annot"]["submit_button"]
-    # Format report and annotation file name
-    filename_report = session["filename"] + ".txt"
-    filename_annot = session["filename"] + ".json"
     # Write the histology report to file with first the basic informations
-    f = open("results/" + filename_report, "w")
-    f.write("Prenom_Patient\t" + prenom_patient + "\n")
-    f.write("Nom_Patient\t" + nom_patient + "\n")
-    f.write("ID_Patient\t" + id_patient + "\n")
-    f.write("Redacteur_histo\t" + redacteur_rapport + "\n")
-    f.write("Diagnostic\t" + diag + "\n")
-    # Then we write each histology feature values with a loop
-    for i in session["info_annot"]:
-        f.write(i + "\t" + session["info_annot"][i] + "\n")
+    with open(os.path.join("results/", session["filename"] + ".txt"),
+              "w") as f:
+        f.write("Prenom_Patient\t" + session["patient_prenom"] + "\n")
+        f.write("Nom_Patient\t" + session["patient_nom"] + "\n")
+        f.write("ID_Patient\t" + session["patient_ID"] + "\n")
+        f.write("Redacteur_histo\t" + session["expert_name"] + "\n")
+        f.write("Diagnostic\t" + session["diagnostic"] + "\n")
+        # Then we write each histology feature values with a loop
+        for i in session["feature"]:
+            f.write(i + "\t" + session["feature"][i] + "\n")
+
     # Finally we render the results page
     return render_template("results.html",
-                           data=session["info_annot"],
-                           prenom_patient=prenom_patient,
-                           nom_patient=nom_patient,
-                           id_patient=id_patient,
-                           redacteur_rapport=redacteur_rapport,
-                           filename_report=filename_report,
-                           filename_annot=filename_annot)
+                           feature_info=session["feature"],
+                           prenom_patient=session["patient_prenom"],
+                           nom_patient=session["patient_nom"],
+                           id_patient=session["patient_ID"],
+                           redacteur_rapport=session["expert_name"],
+                           filename_report=session["filename"] + ".txt",
+                           filename_annot=session["filename"] + ".json")
 
 
 if __name__ == "__main__":
