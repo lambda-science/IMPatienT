5bd9f1a3be697fb454233d9ce1d8251eb09f26b6
Corentin <7775862-Corentin_7@users.noreply.gitlab.com>
2020-12-17 11:42:41 +0100

    Table Patient and Images

--- app/models.py
+++ app/models.py
@@ -8,4 +8,28 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User {}> <Email {}>'.format(self.username, self.email)
+        return '<User {} Email {}>'.format(self.username, self.email)
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_name = db.Column(db.String(140), index=True)
+    expert_name = db.Column(db.Integer, db.ForeignKey('user.id'))
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'))
+    image_binary = db.Column(db.LargeBinary)
+    diagnostic = db.Column(db.String(140), index=True)
+    report_text = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Image Name {} Patient {}>'.format(self.image_name,
+                                                   self.patient_id)
+
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_firstname = db.Column(db.String(140))
+    patient_name = db.Column(db.String(140), index=True)
+
+    def __repr__(self):
+        return '<Patient {} {} {}>'.format(self.id, self.patient_firstname,
+                                           self.patient_name)
