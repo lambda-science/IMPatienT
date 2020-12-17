b40d0896115346e4121ec61372e9eda75ad9d9c8
Corentin <7775862-Corentin_7@users.noreply.gitlab.com>
2020-12-17 10:57:54 +0100

    Added migration tools for DB

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import os
 import app.histofunc as Histofunc
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'myverylongsecretkey'
     ALLOWED_EXTENSIONS = ["tif", "tiff", "png", "jpg", "jpeg"]
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
-    REPORT_FOLDER = os.path.join(os.getcwd(), "results")
+    UPLOAD_FOLDER = os.path.join(basedir, "uploads")
+    REPORT_FOLDER = os.path.join(basedir, "results")
     FEATURE_LIST = Histofunc.create_feature_list(
         os.path.join("config", "config_ontology.tsv"))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
