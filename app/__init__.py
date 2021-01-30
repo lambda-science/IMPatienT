6b04a5ff5ebc7afaca4e46c35aaeac53c5d1550d
Corentin <7775862-Corentin_7@users.noreply.gitlab.com>
2021-01-30 12:35:31 +0100

    update for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -70,22 +70,23 @@ def create_app(config_class=Config):
 
         # Create logs folder to log errors & log them.
         if app.config['LOG_TO_STDOUT']:
-                    stream_handler = logging.StreamHandler()
-                    stream_handler.setLevel(logging.INFO)
-                    app.logger.addHandler(stream_handler)
-                else:
-                    if not os.path.exists('logs'):
-                        os.mkdir('logs')
-                    file_handler = RotatingFileHandler('logs/histoannot.log',
-                                                    maxBytes=10240, backupCount=10)
-                    file_handler.setFormatter(logging.Formatter(
-                        '%(asctime)s %(levelname)s: %(message)s '
-                        '[in %(pathname)s:%(lineno)d]'))
-                    file_handler.setLevel(logging.INFO)
-                    app.logger.addHandler(file_handler)
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/histoannot.log',
+                                               maxBytes=10240,
+                                               backupCount=10)
+            file_handler.setFormatter(
+                logging.Formatter('%(asctime)s %(levelname)s: %(message)s '
+                                  '[in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
 
-                app.logger.setLevel(logging.INFO)
-                app.logger.info('Microblog startup')
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('HistoAnnot startup')
 
     return app
 
