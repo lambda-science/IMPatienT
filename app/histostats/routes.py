6299f58e56aac4f3c060a91151859aeeca43d85c
Corentin M <corentin.meyer@etu.unistra.fr>
2021-05-05 17:59:00 +0200

    Simple Stat page for histo features

--- app/histostats/routes.py
+++ app/histostats/routes.py
@@ -1,11 +1,25 @@
 import json
+import os
 from flask_login import current_user, login_required
-from flask import render_template, request, flash, redirect, url_for
-from app import db
+from flask import render_template, current_app
 from app.histostats import bp
 
 
 @bp.route("/histostats", methods=["GET", "POST"])
 def statsindex():
     """Page for histo statistics page."""
-    return render_template("histostats_index.html")
+    stat_per_gene_file = open(
+        os.path.join(current_app.config["CONFIG_FOLDER"], "stat_per_gene.json")
+    )
+    stat_per_gene = json.load(stat_per_gene_file)
+    stat_per_diag_file = open(
+        os.path.join(current_app.config["CONFIG_FOLDER"], "stat_per_diag.json")
+    )
+    stat_per_diag = json.load(stat_per_diag_file)
+    stat_per_gene = sorted(stat_per_gene.items())
+    stat_per_diag = sorted(stat_per_diag.items())
+    return render_template(
+        "histostats_index.html",
+        stat_per_gene=stat_per_gene,
+        stat_per_diag=stat_per_diag,
+    )
