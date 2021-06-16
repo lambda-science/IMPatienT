015c9c481d305d735c8521f9c96d5a7848de75a2
Corentin M <corentin.meyer@etu.unistra.fr>
2021-06-16 17:23:25 +0200

    deleted print

--- app/dashapp/routes.py
+++ app/dashapp/routes.py
@@ -6,6 +6,5 @@ from app.dashapp import bp
 @bp.route(bp.static_folder + "/<path:filename>")
 @login_required
 def image_folder_seg(filename):
-    print()
     """Serve files located in patient subfolder inside folder"""
     return send_from_directory(bp.static_folder, filename)
