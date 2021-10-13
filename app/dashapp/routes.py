ceeb8d539cb2fd0017eb0551efbe478a534cb781
LS <20109584+lambda-science@users.noreply.github.com>
2021-10-13 10:54:25 +0200

    Remove login requiered for image as it is bugged yet

--- app/dashapp/routes.py
+++ app/dashapp/routes.py
@@ -3,15 +3,15 @@ from flask_login import login_required
 from app.dashapp import bp
 
 
-@bp.route(bp.static_folder + "/<path:filename>")
-@login_required
-def image_folder_seg(filename):
-    """Serve files located in patient subfolder inside folder"""
-    return send_from_directory(bp.static_folder, filename)
+# @bp.route(bp.static_folder + "/<path:filename>")
+# @login_required
+# def image_folder_seg(filename):
+#     """Serve files located in patient subfolder inside folder"""
+#     return send_from_directory(bp.static_folder, filename)
 
 
 @bp.route("/data/<path:filename>")
-@login_required
+#@login_required
 def data_folder(filename):
     """Serve files located in patient subfolder inside folder"""
     return send_from_directory(current_app.config["DATA_FOLDER"], filename)
