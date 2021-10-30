71384a1bf4f21060aad79694f894fba2bd2ab5cf
LS <20109584+lambda-science@users.noreply.github.com>
2021-10-30 21:04:36 +0200

    Fixed a docker bug (I hate it)

--- docker/boot.sh
+++ docker/boot.sh
@@ -14,4 +14,14 @@ while true; do
     echo Deploy command failed, retrying in 5 secs...
     sleep 5
 done
+
+while true; do
+    python myoxia.py
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+
 exec gunicorn -b :5000 --access-logfile - --error-logfile - --timeout 900 --workers 4 --threads 8 --preload myoxia:app
