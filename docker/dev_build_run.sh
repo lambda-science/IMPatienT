d5d3ed7da149955c9b0440d75ad5ab9292553f4c
LS <20109584+lambda-science@users.noreply.github.com>
2021-11-28 15:04:33 +0100

    From myoxia to EHRoes

--- docker/dev_build_run.sh
+++ docker/dev_build_run.sh
@@ -1,2 +1,2 @@
 docker build . -f Dockerfile.dev -t ehroesdev:latest
-docker run --rm --name ehroesdev -p 5000:5000 -v $(pwd):/home/ehroes/code  ehroesdev:latest
+docker run --rm -it --name ehroesdev -p 5000:5000 -v $(pwd):/home/ehroes/code  ehroesdev:latest
