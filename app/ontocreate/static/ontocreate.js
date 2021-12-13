a44c87a5303a8d02f41e357d936b930957271a0f
LS <20109584+lambda-science@users.noreply.github.com>
2021-12-13 17:27:50 +0100

    proper comment for JS files

--- app/ontocreate/static/ontocreate.js
+++ app/ontocreate/static/ontocreate.js
@@ -1,4 +1,7 @@
+// Get data stored in data-url meta tag
 var data_url = $("#data-url").data();
+
+// Generate all Tagify object for tag input in the form.
 var input = document.querySelector("input[id=synonymes]");
 var input_tag = new Tagify(input);
 // var input2 = document.querySelector("input[id=gene]");
@@ -14,6 +17,11 @@ var input6_tag = new Tagify(input6);
 var input7 = document.querySelector("input[id=correlates_with]");
 var input7_tag = new Tagify(input7);
 
+/**
+ * Create a new node ID incrementally from all node ID as MHO:XXXXXX
+ * @param {array}     id_list   list of all current standard vocabulary terms ID
+ * @returns {string}  id        id of the new term
+ */
 function ontology_ID(id_list) {
   id = id_list.sort()[id_list.length - 2].substring(4);
   id = parseInt(id);
@@ -22,13 +30,18 @@ function ontology_ID(id_list) {
   return id;
 }
 
+/**
+ * Initilize JSTree
+ * Load a custom create_node function using our own function and settings default node data
+ * Load Data from the data_url.jstree meta tag
+ * Activate all required plugins
+ */
 $("#jstree")
   .bind("create_node.jstree", function (event, data) {
     var v = $("#jstree").jstree(true).get_json("#", { flat: true });
     var id_list = v.map(({ id }) => id);
     var newId = ontology_ID(id_list);
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    // data.node.data = { description: "", genes: "", synonymes: "", phenotype: "", phenotype_datamined: "", gene_datamined: "", alternative_language: "", correlates_with: "" };
     data.node.data = {
       description: "",
       synonymes: "",
@@ -59,6 +72,7 @@ $("#jstree")
     ],
   });
 
+// Search function for the JSTree search bar
 var to = false;
 $("#plugins4_q").keyup(function () {
   if (to) {
@@ -70,7 +84,7 @@ $("#plugins4_q").keyup(function () {
   }, 250);
 });
 
-// Prefill Form from JSON
+// When a JSTree is selected: dynamically update the form about term details.
 $("#jstree").on("select_node.jstree", function (e, data) {
   $("input[id=onto_id_ext]").val(data.node.id);
   $("input[id=onto_name]").val(data.node.text);
@@ -92,7 +106,11 @@ $("#jstree").on("select_node.jstree", function (e, data) {
   $("textarea[id=description]").val(data.node.data.description) || "";
 });
 
-// Register form data to JSON file
+/**
+ * When the "update details" button is clicked: update the data of the selected node
+ * Get form data, insert it in the JSTree node.data object
+ * Call the save_tree function
+ */
 function update_node_data() {
   var node_id = $("#jstree").jstree(true).get_selected();
   var node = $("#jstree").jstree(true).get_node(node_id);
@@ -108,6 +126,9 @@ function update_node_data() {
   node.data.description = $("textarea[id=description]").val();
   save_tree();
 }
+/**
+ * Get the current JStree Json data and make an AJAX Request to update the .JSON file
+ */
 function save_tree() {
   var v = $("#jstree").jstree(true).get_json("#", { flat: true });
   myJSON = JSON.stringify(v);
@@ -120,6 +141,11 @@ function save_tree() {
   });
 }
 
+/**
+ * Function to extract the tag list from a given "TagList" input as a string.
+ * @param {string} ID of the input tag
+ * @returns {string} list of all tags in the input tag separated by ","
+ */
 function get_taglist(input_id) {
   list_tag = [];
   if ($(input_id).val() === "") {
