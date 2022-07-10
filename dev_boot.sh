ad8fd5397b8c15b6b87763277d95161816069458
Corentin <>
2022-07-10 19:27:39 +0000

    upd

--- dev_boot.sh
+++ dev_boot.sh
@@ -5,6 +5,11 @@ if [ ! -f $FILE ]; then
     cp data/ontology/ontology_default.json data/ontology/ontology.json
 fi
 
+DB="data/database/app.db"
+if [ ! -f $DB ]; then
+    cp data/database/app.db.demo data/database/app.db
+fi
+
 # Check if running in a container or not.
 if [ -f /.dockerenv ]; then
     flask db upgrade
